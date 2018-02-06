a8e1a1eaf4d8ea4fde8717ed62124694f1d130ca
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-02-06 16:35:04 +0100

    including Rob movies in shape index analysis

--- code/calculateShapeIndexFromVertices.m
+++ code/calculateShapeIndexFromVertices.m
@@ -5,7 +5,7 @@ function [ medianShapeIndex,averageShapeIndex,totalValidCells] = calculateShapeI
     BW=zeros(size(L_img));
     BW(L_img==0)=1;
     
-    if max(max(bwlabel(1-BW)))<20
+    if max(max(bwlabel(1-BW)))<80
        W=watershed(logical(BW),4);
     else
         W=watershed(logical(BW),8);
@@ -22,13 +22,18 @@ function [ medianShapeIndex,averageShapeIndex,totalValidCells] = calculateShapeI
     lastRowCells=unique(W(end,1:end));
     firstColumnCells=unique(W(1:end,1))';
     lastColumnCells=unique(W(1:end,end))';
-    outRoiCells=unique(W((1-BW)==0))';%added for neo_samples
+    %outRoiCells=unique(W((1-BW)==0))';%added for neo_samples
     
-    noValidCells=unique([firstRowCells,lastRowCells,firstColumnCells,lastColumnCells,outRoiCells]);
+    noValidCells=unique([firstRowCells,lastRowCells,firstColumnCells,lastColumnCells]);%,outRoiCells]);
     
     noValidCells=noValidCells(noValidCells~=0);
-    if noValidCells==1
-        noValidCells=[noValidCells;neighs{noValidCells}];
+    if length(noValidCells)<30
+        areaCell=regionprops(W,'Area');
+        areaCell=cat(1,areaCell.Area);
+        per80 = prctile(areaCell,80);
+        noValidBigArea=areaCell>5*per80;
+        noValidCells=find(noValidBigArea==1);
+        noValidCells=unique([noValidCells;vertcat(neighs{noValidCells})]);
     end
     validCells=setxor(numCells,noValidCells);
     
